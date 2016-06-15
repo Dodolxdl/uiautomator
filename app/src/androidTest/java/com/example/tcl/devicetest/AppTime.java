74d0843b1fe2030ac0c24459eb784769bd3674f0
zhangrui <zhangrui90158@163.com>
2016-06-15 13:09:38 +0800

    添加：热启动10次平均时间

--- app/src/androidTest/java/com/example/tcl/devicetest/AppTime.java
+++ app/src/androidTest/java/com/example/tcl/devicetest/AppTime.java
@@ -20,6 +20,7 @@ import org.junit.runner.RunWith;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -32,6 +33,7 @@ public class AppTime {
     private Instrumentation instrumentation;
     private UiDevice device;
     private Context context;
+    private PackageManager pm;
 
 
     @Before
@@ -39,6 +41,10 @@ public class AppTime {
         instrumentation = InstrumentationRegistry.getInstrumentation();
         device = UiDevice.getInstance(instrumentation);
         context = InstrumentationRegistry.getContext();
+        pm = context.getPackageManager();
+
+
+
 
     }
     @After
@@ -49,21 +55,19 @@ public class AppTime {
         device.pressHome();
     }
     @Test
-    public void getTime(){
-        PackageManager pm = context.getPackageManager();
+    public void getCoolTime(){
         List<PackageInfo> packageInfos = pm.getInstalledPackages(0);
         List<ApplicationInfo> applicationInfos = pm.getInstalledApplications(0);
         Intent mainIntent = new Intent(Intent.ACTION_MAIN);
         mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-
-        List<ResolveInfo> resolveInfos = pm.queryIntentActivities(mainIntent,0);
-        Intent intent = new Intent();
-        File file1 = new File(Environment.getExternalStorageDirectory(),"Total.txt");
+        File file1 = new File(Environment.getExternalStorageDirectory(),"CoolTime.txt");
         if(file1.exists()){
             file1.delete();
         }
         FileWriter fos =null;
         FileWriter fos1 = null;
+        List<ResolveInfo> resolveInfos = pm.queryIntentActivities(mainIntent,0);
+        Intent intent = new Intent();
         for(ResolveInfo info:resolveInfos){
             try {
                 fos1 = new FileWriter(file1,true);
@@ -73,7 +77,7 @@ public class AppTime {
                 Matcher matcher = pattern.matcher(waitTime);
                 String activityName = info.activityInfo.name;
                 if(matcher.find()){
-                    fos1.write(activityName + ":" + matcher.group(0) + "\n");
+                    fos1.write(activityName + "冷启动时间" + matcher.group(0) + "\n");
                     sleep(5000);
                 }
 
@@ -103,5 +107,90 @@ public class AppTime {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+    @Test
+    public void getHootTime(){
+        List<PackageInfo> packageInfos = pm.getInstalledPackages(0);
+        List<ApplicationInfo> applicationInfos = pm.getInstalledApplications(0);
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> resolveInfos = pm.queryIntentActivities(mainIntent,0);
+        Intent intent = new Intent();
+        Pattern pattern = Pattern.compile("WaitTime:\\s\\d*");
+        File file = new File(Environment.getExternalStorageDirectory(),"HootTime.txt");
+        if(file.exists()){
+            file.delete();
+        }
+        FileWriter fw = null;
+        for(ResolveInfo info:resolveInfos){
+            List<String> time = new ArrayList<String>();
+            try {
+                for(int i =0;i<10;i++){
+                    String[] str = info.toString().split(" ");
+                    String waitTime = device.executeShellCommand("am start -W "+ str[1]);
+                    Matcher matcher = pattern.matcher(waitTime);
+                    String packageName = info.activityInfo.packageName;
+                    if(matcher.find()){
+                        String[] string = matcher.group().split(":");
+                        String finalTime = string[1].trim();
+                        time.add(finalTime);
+                        device.pressHome();
+                        sleep(1000);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String[] str = getSting(time);
+            int[] ints = getInt(str);
+            double averageTime = getAverageTime(ints);
+//            String finalTime = averageTime+" ";
+            String finalTime = String.valueOf(averageTime);
+            String packageName = info.activityInfo.packageName;
+            try {
+                fw = new FileWriter(file,true);
+                fw.write(packageName + ":");
+                for(String string:str){
+                    fw.write(string+" ");
+                }
+                fw.write("10次平均时间为："+ finalTime + "ms");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                fw.write("\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if(fw!=null){
+                    try {
+                        fw.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+    //求平均数
+    public double getAverageTime(int[] arrays){
+        double sum=0;
+        for(int i=0;i<arrays.length;i++){
+            sum+=arrays[i];
+        }
+        return sum/10;
+    }
+    //将List集合转化为String数组
+    public String[] getSting(List<String> list){
+        String[] arraysList = list.toArray(new String[list.size()]);
+        return arraysList;
+    }
+    //将String数组转化为int数组
+    public int[] getInt(String[] strings){
+        int[] ints = new int[strings.length];
+        for(int i=0;i<strings.length;i++){
+            ints[i] = Integer.parseInt(strings[i]);
+        }
+        return  ints;
     }
 }
